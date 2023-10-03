902984099ab6f9c06c94ab793e49c6d393a6a5b5
ismailcodenext <engr.ismail348@gmail.com>
2023-10-03 20:58:11 +0600

    assigment 4

--- assignment-3/task_5.php
+++ assignment-3/task_5.php
@@ -1,12 +1,21 @@
 <?php
-$char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
-function generatePassword($length)
-{
-    global $char;
-    return substr(str_shuffle($char), 0, $length);
-}
+// $char = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+';
+// function generatePassword($length)
+// {
+//     global $char;
+//     return substr(str_shuffle($char), 0, $length);
+// }
 
-$password = generatePassword(12);
-echo "Generated Password: $password";
+// $password = generatePassword(12);
+// echo "Generated Password: $password";
+
+// $numbers = [1, 2, 3, 4, 5];
+
+// $result = count($numbers);
+// echo $result;
+
+$fruits = ['apple', 'banana', 'cherry'];
+
+echo $fruits[1];
 
 ?>
